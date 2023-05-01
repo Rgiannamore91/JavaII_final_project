f9e9828e2907dc3a22c34ff210c81b0f00989eca
DESKTOP-2FH8J2L\houzi <houzidalao91@gmail.com>
2023-05-01 19:55:57 -0400

    Fill out and test month and day objects.

--- src/Month.java
+++ src/Month.java
@@ -1,2 +1,37 @@
 public class Month {
+    private int monthNumber;
+    private String monthName;
+    private int numberOfDays;
+
+    public Month(int monthNumber, String monthName, int numberOfDays){
+        this.monthNumber = monthNumber;
+        this.monthName = monthName;
+        this.numberOfDays = numberOfDays;
+    }
+
+    public int getMonthNumber(){
+        return monthNumber;
+    }
+    public final void setMonthNumber(int monthNumber){
+        this.monthNumber = monthNumber;
+    }
+
+    public String getMonthName(){
+        return monthName;
+    }
+    public final void setMonthName(String monthName){
+        this.monthName = monthName;
+    }
+
+    public int getNumberOfDays(){
+        return numberOfDays;
+    }
+    public final void setNumberOfDays(int numberOfDays){
+        this.numberOfDays=numberOfDays;
+    }
+    
+    @Override
+    public String toString(){
+        return String.format("%d : %s \nNumber of days: %d", monthNumber, monthName, numberOfDays);
+    }
 }
